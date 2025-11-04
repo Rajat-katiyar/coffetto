8d87d22e794ac3153b95fcba8b6374de5f616ac8
rajat <rajatkatiyar157@gmail.com>
2025-11-05 00:41:37 +0530

    Fix Vercel build: Exclude Vite files from TypeScript and Webpack compilation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Exclude Vite config files from compilation
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/vite.config.*', '**/vite-env.d.ts'],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
